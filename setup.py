0b00fe2c12491e67a83218e41192514f06dcad05
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-02-22 18:40:54 -0500

    - added to setup script

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ files['FreeImage 3.15.1'] = ['http://dl.bintray.com/jarrettchisholm/generic/free
 files['AssImp 3.0.1270'] = ['http://dl.bintray.com/jarrettchisholm/generic/assimp_3.0.1270_linux_x64.tar.gz', 'assimp_3.0.1270_linux_x64.tar.gz']
 files['CEF3 3.1650.1562'] = ['http://dl.bintray.com/jarrettchisholm/generic/cef3_3.1650.1562_linux_x64.tar.gz', 'cef3_3.1650.1562_linux_x64.tar.gz']
 
+def installSystemDependencies():
+	"""Install any system libraries we require"""
+	pass
+
 def download():
-	"""Downloads files"""
+	"""Download external library files"""
 	for k in files.keys():
 		print('Downloading ' + k)
 		
@@ -35,7 +39,7 @@ def download():
 			f.write( bytearray(data) )
 
 def extract():
-	"""Extract files"""
+	"""Extract external library files"""
 	
 	for k in files.keys():
 		print('Extracting ' + k)
@@ -43,6 +47,12 @@ def extract():
 		with tarfile.TarFile.open(files[k][1], 'r:gz') as f:
 			f.extractall()
 
+def install():
+	"""Install the downloaded libraries locally"""
+	pass
 
+
+installSystemDependencies()
 download()
 extract()
+install()
