75957ed2fc893238efb524e6576a7dd1c574ea9b
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-02-22 18:36:15 -0500

    - worked on setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,48 @@
+import urllib2
+import tarfile
+import gzip
+import sys
 
 
+files = dict()
+files['Boost 1.55'] = ['http://dl.bintray.com/jarrettchisholm/generic/boost_1.55_x64.tar.gz', 'boost_1.55_linux_x64.tar.gz']
+files['FreeImage 3.15.1'] = ['http://dl.bintray.com/jarrettchisholm/generic/freeimage_3.15.1_linux_x64.tar.gz', 'freeimage_3.15.1_linux_x64.tar.gz']
+files['AssImp 3.0.1270'] = ['http://dl.bintray.com/jarrettchisholm/generic/assimp_3.0.1270_linux_x64.tar.gz', 'assimp_3.0.1270_linux_x64.tar.gz']
+files['CEF3 3.1650.1562'] = ['http://dl.bintray.com/jarrettchisholm/generic/cef3_3.1650.1562_linux_x64.tar.gz', 'cef3_3.1650.1562_linux_x64.tar.gz']
 
-http://dl.bintray.com/jarrettchisholm/generic/boost_1.55.tar.gz
-http://dl.bintray.com/jarrettchisholm/generic/freeimage_3.15.1_linux_x64.tar.gz
-http://dl.bintray.com/jarrettchisholm/generic/assimp_3.0.1270_linux_x64.tar.gz
+def download():
+	"""Downloads files"""
+	for k in files.keys():
+		print('Downloading ' + k)
+		
+		response = urllib2.urlopen(files[k][0])
+		
+		contentLength = response.headers["Content-Length"]
+		
+		data = ''
+		chunk = 4096
+		while 1:
+			d = response.read(chunk)
+			if not d:
+				print("\n")
+				break
+			data += d
+			
+			sys.stdout.write( "\rRead {0} / {1} KB".format((len(data)/1000), int(contentLength)/1000) )
+			sys.stdout.flush()
+		
+		with open(files[k][1], 'w') as f:
+			f.write( bytearray(data) )
 
-http://www.magpcss.net/cef_downloads/index.php?file=cef_binary_3.1650.1562_linux64.7z
+def extract():
+	"""Extract files"""
+	
+	for k in files.keys():
+		print('Extracting ' + k)
+		
+		with tarfile.TarFile.open(files[k][1], 'r:gz') as f:
+			f.extractall()
+
+
+download()
+extract()
