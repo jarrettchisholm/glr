7b8291a377cc85f4a4c587162f93b3ea4384defc
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-04-11 22:05:15 -0400

    - missed files in last commit

---
+++ include/serialize/glm/Vec2.hpp
@@ -0,0 +1,30 @@
+#ifndef GLMVEC2SERIALIZE_H_
+#define GLMVEC2SERIALIZE_H_
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/vector.hpp>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
+BOOST_SERIALIZATION_SPLIT_FREE(glm::vec2)
+
+namespace boost
+{
+namespace serialization
+{
+
+template<class Archive> void save(Archive& ar, const glm::vec2& v, unsigned int version)
+{
+	ar & v.x & v.y;
+}
+
+template<class Archive> void load(Archive& ar, glm::vec2& v, unsigned int version)
+{
+	ar & v.x & v.y;
+}
+
+}
+}
+
+#endif /* GLMVEC2SERIALIZE_H_ */
---
+++ include/serialize/glm/Vec4.hpp
@@ -0,0 +1,30 @@
+#ifndef GLMVEC4SERIALIZE_H_
+#define GLMVEC4SERIALIZE_H_
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/vector.hpp>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
+BOOST_SERIALIZATION_SPLIT_FREE(glm::vec4)
+
+namespace boost
+{
+namespace serialization
+{
+
+template<class Archive> void save(Archive& ar, const glm::vec4& v, unsigned int version)
+{
+	ar & v.w & v.x & v.y & v.z;
+}
+
+template<class Archive> void load(Archive& ar, glm::vec4& v, unsigned int version)
+{
+	ar & v.w & v.x & v.y & v.z;
+}
+
+}
+}
+
+#endif /* GLMVEC4SERIALIZE_H_ */
