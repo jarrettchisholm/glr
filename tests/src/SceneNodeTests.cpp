ff655e4035cb414767393cfc8e2bc1d5f8b01709
Jarrett Chisholm <j.chisholm@prylynx.com>
2015-05-19 15:41:10 -0400

    - a few test improvements

--- tests/src/SceneNodeTests.cpp
+++ tests/src/SceneNodeTests.cpp
@@ -19,76 +19,12 @@ BOOST_AUTO_TEST_SUITE(sceneNode)
 
 BOOST_AUTO_TEST_CASE(createDeleteSceneNodes)
 {
-    auto p = std::unique_ptr<glr::GlrProgram>( new glr::GlrProgram() );
     
-    p->createWindow();
-    
-    glr::ISceneManager* smgr = p->getSceneManager();
-	
-	
-	// Single create/destroy test
-	glr::ISceneNode* node = smgr->createSceneNode( std::string("test") );
-
-	BOOST_REQUIRE( node != nullptr ); 
-	BOOST_CHECK_EQUAL( smgr->getNumSceneNodes(), 1u );
-	
-	smgr->destroySceneNode( node );
-	
-	BOOST_CHECK_EQUAL( smgr->getNumSceneNodes(), 0u );
-	
-	// Make sure node was actually deleted
-	node = smgr->getSceneNode( std::string("test") );
-	
-	BOOST_REQUIRE( node == nullptr );
-	BOOST_CHECK_EQUAL( smgr->getNumSceneNodes(), 0u );
-	
-	
-	// Create/destroy multiple scene nodes
-	for ( int i=0; i < 30; i++ )
-	{
-		std::stringstream ss;
-		ss << std::string("test_node_") << i;
-		smgr->createSceneNode(  ss.str() );
-	}
-	
-	BOOST_CHECK_EQUAL( smgr->getNumSceneNodes(), 30u );
-	
-	// Make sure we can get all of the scene nodes by name
-	for ( int i=0; i < 30; i++ )
-	{
-		std::stringstream ss;
-		ss << std::string("test_node_") << i;
-		node = smgr->getSceneNode( ss.str() );
-		BOOST_REQUIRE( node != nullptr );
-	}
-	
-	// Make sure we don't get an erroneous node
-	node = smgr->getSceneNode( std::string("test_node_30") );
-	BOOST_REQUIRE( node == nullptr );
-	
-	// Test destroying one of the nodes
-	smgr->destroySceneNode( std::string("test_node_4") );
-	node = smgr->getSceneNode( std::string("test_node_4") );
-	BOOST_REQUIRE( node == nullptr );
-	
-	BOOST_CHECK_EQUAL( smgr->getNumSceneNodes(), 29u );
-	
-	// Test destroying the remaining nodes
-	smgr->destroyAllSceneNodes();
-	
-	BOOST_CHECK_EQUAL( smgr->getNumSceneNodes(), 0u );
 }
 
 BOOST_AUTO_TEST_CASE(sceneNodeMovement)
 {
-    auto p = std::unique_ptr<glr::GlrProgram>( new glr::GlrProgram() );
-    
-    p->createWindow();
-    
-    glr::ISceneManager* smgr = p->getSceneManager();
-	
-	glr::ISceneNode* node = smgr->createSceneNode( std::string("test") );
-
+    auto node = std::unique_ptr< glr::BasicSceneNode >( new glr::BasicSceneNode(glr::Id(1), nullptr) );
 
 	// Scale test
 	node->setScale(0.1f, 0.1f, 0.1f);
